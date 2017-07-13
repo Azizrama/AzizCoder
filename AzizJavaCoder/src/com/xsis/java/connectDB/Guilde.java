455709c50203fc093533ac58b0abf2cb2eb84937
Administrator <Administrator@10.8.106.70>
2017-07-13 14:44:09 +0700

    update guild coonect java to oracle

---
+++ AzizJavaCoder/src/com/xsis/java/connectDB/Guilde.java
@@ -0,0 +1,41 @@
+package com.xsis.java.connectDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Guilde {
+	
+	public static void main(String[] args) {
+		System.out.println("----------Oracle Connection----------");
+		try{
+			   // register oracle driver
+			   Class.forName("oracle.jdbc.driver.OracleDriver");
+			   
+			  }catch(ClassNotFoundException ex){
+			   System.out.println("Oracle Driver not found");
+			   ex.printStackTrace();
+			   return;
+			  }                                                 
+			  
+			  System.out.println("Oracle Driver has been successfully added");
+			  
+			  // create object connection
+			  Connection conn = null;
+			  
+			  try{        
+			   // make connection with oracle database
+			   // parameter : url , user name , password
+			   conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr");
+			  }catch(SQLException ex ){
+			   System.out.println("Connection Failed !!");
+			   ex.printStackTrace();
+			   return;
+			  }
+			  
+			  System.out.println("Successfully to connect oracle database");
+
+	}
+}
+
+
